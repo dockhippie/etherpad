e8edecf3e32f7d05c7fb901eaa661704a2cc6a36
Benjamin Schweizer <cxcv@cxcv.de>
2019-03-03 20:02:16 +0100

    remmoved debug flag

--- rootfs/etc/s6/etherpad/custom.d/15-config.sh
+++ rootfs/etc/s6/etherpad/custom.d/15-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "> writing etherpad config"
-/usr/bin/templater -d -p etherpad -o /srv/www/settings.json /etc/templates/settings.tmpl
+/usr/bin/templater -p etherpad -o /srv/www/settings.json /etc/templates/settings.tmpl
 
 if [[ $? -ne 0 ]]
 then
